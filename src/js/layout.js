e87cb969563b0829f7acfbe38a5ed373b1a7d3cb
Evan Otero <Evan.otero19@gmail.com>
2019-10-31 16:26:37 +0000

    created favorites page cards

--- src/js/layout.js
+++ src/js/layout.js
@@ -5,6 +5,7 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
+import Favorites from "./views/Favorites.js";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -23,7 +24,7 @@ export const Layout = () => {
 					<Navbar />
 					<Switch>
 						<Route exact path="/" component={Home} />
-						<Route path="/favorites" />
+						<Route path="/favorites" component={Favorites} />
 						<Route path="/details" />
 						<Route path="/profile" />
 						<Route path="/editprofile" />
