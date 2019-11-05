f91efbde639e6bf6fabc18577bfc7d8804ee57de
Evan Otero <Evan.otero19@gmail.com>
2019-11-05 01:53:30 +0000

    fixed footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,21 +2,19 @@ import React, { Component } from "react";
 import "../../styles/home.scss";
 
 export const Footer = () => (
-	<div>
-		<div className="card text-center fixed-bottom bg-light">
-			<div className="card-header">
-				<ul className="nav nav-pills card-header-pills d-flex justify-content-center">
-					<li className="nav-item">
-						<a className="nav-link fas fa-search" href="#" />
-					</li>
-					<li className="nav-item">
-						<a className="nav-link fas fa-paw" href="#" />
-					</li>
-					<li className="nav-item">
-						<a className="nav-link fas fa-map-pin" href="#" />
-					</li>
-				</ul>
-			</div>
+	<div className="card text-center fixed-bottom bg-light">
+		<div className="card-header">
+			<ul className="nav nav-pills card-header-pills d-flex justify-content-around bottomnav">
+				<li className="nav-item">
+					<a className="nav-link fas fa-search" href="#" />
+				</li>
+				<li className="nav-item">
+					<a className="nav-link fas fa-paw" href="#" />
+				</li>
+				<li className="nav-item">
+					<a className="nav-link fas fa-map-pin" href="#" />
+				</li>
+			</ul>
 		</div>
 	</div>
 );
